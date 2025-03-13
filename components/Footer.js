f6414c2f5dc61fe06c12a33ac81ec646e84197ff
AbayomiOlaoye <mindprepopps@gmail.com>
2025-03-13 10:31:03 +0100

    Update Footer component layout for improved spacing and styling

--- components/Footer.js
+++ components/Footer.js
@@ -13,9 +13,9 @@ const Footer = () => {
 
   return (
     <footer className='bg-bg w-full footer'>
-      <div className='mx-auto pt-10 p-5 px-5 md:px-[5%]'>
+      <div className='mx-auto pt-12 p-5 px-5 md:px-[5%]'>
         <div className='flex gap-10 items-start max-lg:gap-6'>
-          <section className='flex flex-col gap-3'>
+          <section className='flex flex-col gap-3 md:w-[40%]'>
             <Link href="/" className="flex items-center cursor-pointer font-ubuntu text-white md:text-xl font-bold">
               <Image src="/ever.svg" alt="Ever Journal" width={30} height={30} />
               Ever Journal
@@ -24,8 +24,8 @@ const Footer = () => {
               Ever Journal is your personal biographer. We help you embark on a journey of self-reflection, rediscovery, and storytelling.
             </small>
           </section>
-          <section className='block'>
-            <h2 className='text-[#505050] pb-2 text-h5 font-tiempos'>HELP</h2>
+          <section className=''>
+            <h2 className='text-silver pb-2'>HELP</h2>
             <div className='flex flex-col gap-2 text-[15] text-[#fff]'>
               <Link href="/about" className='cursor-pointer transition-all min-w-fit hover:text-silver'>
                 About
@@ -45,8 +45,8 @@ const Footer = () => {
             </div>
           </section>
           <section>
-            <h2 className='text-[#505050] pb-2 text-h5'>LEGALS</h2>
-            <div className='flex text-[15] flex-col gap-2 text-[#fff]'>
+            <h2 className='text-silver pb-2'>LEGALS</h2>
+            <div className='flex flex-col gap-2 text-[#fff]'>
               <Link href="/terms-and-conditions" className='cursor-pointer transition-all hover:text-silver'>
                 Terms & Conditions
               </Link>
@@ -56,7 +56,7 @@ const Footer = () => {
             </div>
           </section>
         </div>
-        <div className='w-full h-[1px] bg-[#fff] my-8 mb-4'></div>
+        <div className='w-full h-[1px] bg-[#fff] my-12 mb-4'></div>
         <small className='flex items-center justify-center gap-1 text-white py-2'>
           &copy;
           <span className='text-[14px] lg:text-[15px] text-[#fff]'>
