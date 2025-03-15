fa867caa8f65d2047b91094e08ef6814ae5d0a20
AbayomiOlaoye <mindprepopps@gmail.com>
2025-03-15 09:55:07 +0100

    Secure key in firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -8,12 +8,6 @@ const firebaseConfig = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  // apiKey: "AIzaSyD0aMqZ9fk5Bveg9lcYtvLgG6iU5ey1FWc",
-  // authDomain: "ever-journal-dev.firebaseapp.com",
-  // projectId: "ever-journal-dev",
-  // storageBucket: "ever-journal-dev.firebasestorage.app",
-  // messagingSenderId: "669692913786",
-  // appId: "1:669692913786:web:89650fa4cef71a5c7573c3"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
